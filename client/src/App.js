4bd5cbf3368de4b5bdf824a63b014b457cf25243
chagvv <chagvv@naver.com>
2021-04-09 15:07:25 +0900

    Add Create Customer Action

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import Customer from "./components/Customer";
+import CustomerAdd from "./components/CustomerAdd";
 import "./App.css";
 import React from "react";
 import Paper from "@material-ui/core/Paper";
@@ -8,22 +9,40 @@ import TableHead from "@material-ui/core/TableHead";
 import TableBody from "@material-ui/core/TableBody";
 import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
+import CircularProgress from "@material-ui/core/CircularProgress";
 import { withStyles } from "@material-ui/core/styles";
 
+/*
+1) constructor()
+
+2) componentWillMount()
+
+3) render()
+
+4) componentDidMout()
+*/
+
+/*
+props or state =>  shouldComponentUpdate()
+*/
 const styles = (theme) => ({
   root: {
     width: "100%",
-    marginTop: theme.spacing.unit * 3,
+    marginTop: theme.spacing(3),
     overflowX: "auto",
   },
   table: {
     minWidth: 1080,
   },
+  progress: {
+    margin: theme.spacing(2),
+  },
 });
 
 class App extends Component {
   state = {
-    customers: [],
+    customers: "",
+    completed: 0,
   };
 
   componentDidMount() {
@@ -41,35 +60,48 @@ class App extends Component {
   render() {
     const { classes } = this.props;
     return (
-      <Paper className={classes.root}>
-        <Table className={classes.table}>
-          <TableHead>
-            <TableRow>
-              <TableCell>번호</TableCell>
-              <TableCell>이미지</TableCell>
-              <TableCell>이름</TableCell>
-              <TableCell>생년월일</TableCell>
-              <TableCell>성별</TableCell>
-              <TableCell>직업</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {this.state.customers.map((c) => {
-              return (
-                <Customer
-                  key={c.id}
-                  id={c.id}
-                  image={c.image}
-                  name={c.name}
-                  birthday={c.birthday}
-                  gender={c.gender}
-                  job={c.job}
-                ></Customer>
-              );
-            })}
-          </TableBody>
-        </Table>
-      </Paper>
+      <div>
+        <Paper className={classes.root}>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>번호</TableCell>
+                <TableCell>이미지</TableCell>
+                <TableCell>이름</TableCell>
+                <TableCell>생년월일</TableCell>
+                <TableCell>성별</TableCell>
+                <TableCell>직업</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {this.state.customers ? (
+                this.state.customers.map((c) => {
+                  return (
+                    <Customer
+                      key={c.id}
+                      id={c.id}
+                      image={c.image}
+                      name={c.name}
+                      birthday={c.birthday}
+                      gender={c.gender}
+                      job={c.job}
+                    ></Customer>
+                  );
+                })
+              ) : (
+                <TableRow>
+                  <TableCell colSpan="6" align="center">
+                    <CircularProgress
+                      className={classes.progress}
+                    ></CircularProgress>
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </Paper>
+        <CustomerAdd></CustomerAdd>
+      </div>
     );
   }
 }
--- server.js
+++ server.js
@@ -3,36 +3,53 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 5000;
 
+const multer = require("multer");
+const upload = multer({ dest: "./upload" });
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+var data = [
+  {
+    id: 1,
+    image: "https://placeimg.com/64/64/any/1",
+    name: "장용호",
+    birthday: "961222",
+    gender: "남자",
+    job: "대학생",
+  },
+  {
+    id: 2,
+    image: "https://placeimg.com/64/64/any/2",
+    name: "홍길동",
+    birthday: "960101",
+    gender: "남자",
+    job: "대학생",
+  },
+  {
+    id: 3,
+    image: "https://placeimg.com/64/64/any/3",
+    name: "이순신",
+    birthday: "200401",
+    gender: "여자",
+    job: "대학생",
+  },
+];
+
 app.get("/api/customers", (req, res) => {
-  res.send([
-    {
-      id: 1,
-      image: "https://placeimg.com/64/64/any/1",
-      name: "장용호",
-      birthday: "961222",
-      gender: "남자",
-      job: "대학생",
-    },
-    {
-      id: 2,
-      image: "https://placeimg.com/64/64/any/2",
-      name: "홍길동",
-      birthday: "960101",
-      gender: "남자",
-      job: "대학생",
-    },
-    {
-      id: 3,
-      image: "https://placeimg.com/64/64/any/3",
-      name: "이순신",
-      birthday: "200401",
-      gender: "여자",
-      job: "대학생",
-    },
-  ]);
+  res.send(data);
 });
 
+app.use("/image", express.static("./upload"));
+app.post("/api/customers", upload.single("image"), (req, res) => {
+  data.push({
+    id: req.body.name,
+    birthday: req.body.birthday,
+    image: "/image/" + req.file.fileName,
+    gender: req.body.gender,
+    job: req.body.job,
+  });
+
+  res.send(data);
+});
 app.listen(port, () => console.log(`Listening on port ${port}`));
